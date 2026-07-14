3589436786a25ab84e21b82ca0c845f52971359d
gabriel <gabrielkamanski0gmail.com>
2026-07-13 22:11:57 -0300

    Atvs feitas

--- index.js
+++ index.js
@@ -4,43 +4,37 @@ const app = express();
 
 app.use(express.json())
 
-// Somar
-app.post("/api/v1/somar", (req, res) => {
+app.post("/api/v1/atv1", (req, res) => {
    const {num1, num2} = req.body
    const resultado = Number(num1) + Number(num2)
   
     res.send({ message : resultado})
-    
 })
-// Subtrair
-app.post("/api/v1/sub", (req, res) => {
-    const {num1, num2} = req.body
-    const resultado = Number(num1) - Number(num2)
-    res.send({message: resultado})
+app.post("/api/v1/atv2", (req, res) => {
+    const {horas, dinheiro} = req.body
+    const resultado = Number(horas) * Number(dinheiro)
+    res.send({ message : resultado})
 })
-
-// Multiplicar
-app.post("/api/v1/mult", (req, res) => {
-    const {num1, num2} = req.body
-    const resultado = Number(num1) * Number (num2)
-    res.send({message : resultado})
+app.post("/api/v1/atv3", (req, res) =>{
+    const {p1, p2, p3, p4, p5} = req.body
+    const media = (Number(p1) + Number(p2) + Number(p3) + Number(p4) + Number(p5))
+    res.send({message: media})
 })
-
-// Dividir
-app.post ("/api/v1/div", (req, res) => {
-    const {num1, num2} = req.body
-    const resultado = Number(num1) / Number(num2)
-    res.send({message : resultado})
+app.post("/api/v1/atv4", (req, res) =>{
+    const temp = req.body
+    const fah = (9 * Number(temp)* + 100 )/5
 })
+app.post("/api/v1/atv5", (req, res) =>{
+    const km = req.body
+    const milhas = Number(km) * 1.60934
 
-
-// Dividir com GET
-//app.get ("/api/v1/div", (req, res) => {
-//    const {num1, num2} = req.query
-//    const resultado = Number(num1) / Number(num2)
-//    res.send({message : resultado})
-//})
-
+})
+app.post("/api/v1/atv6", (req, res) =>{
+     const tempo = req.body
+    const resultado1 = Number(tempo)
+    const resultado2 = Number(tempo) / 60
+    const resultado3 = Number(tempo) / 3600
+})
     app.listen(3000, () => {
     console.log("serve in port 3000")
 })
