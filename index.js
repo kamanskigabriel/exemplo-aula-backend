afc298dab8be7c910708273df938d067fb87b1fb
gabriel <gabrielkamanski0gmail.com>
2026-07-16 19:39:26 -0300

    Commit

--- index.js
+++ index.js
@@ -1,37 +1,32 @@
 import express from "express";
+import cachorro from "./router/cachorro.js"
 
 const app = express();
 
 app.use(express.json())
+app.use('/api/v1/cachorro', cachorro )
 
-// Somar
-app.post("/api/v1/somar", (req, res) => {
-   const {num1, num2} = req.body
-   const resultado = Number(num1) + Number(num2)
-  
-    res.send({ message : resultado})
-    
-})
-// Subtrair
-app.post("/api/v1/sub", (req, res) => {
-    const {num1, num2} = req.body
-    const resultado = Number(num1) - Number(num2)
-    res.send({message: resultado})
-})
+// // Subtrair
+// app.post("/api/v1/sub", (req, res) => {
+//     const {num1, num2} = req.body
+//     const resultado = Number(num1) - Number(num2)
+//     res.send({message: resultado})
+// 
+// })
 
-// Multiplicar
-app.post("/api/v1/mult", (req, res) => {
-    const {num1, num2} = req.body
-    const resultado = Number(num1) * Number (num2)
-    res.send({message : resultado})
-})
+// // Multiplicar
+// app.post("/api/v1/mult", (req, res) => {
+//     const {num1, num2} = req.body
+//     const resultado = Number(num1) * Number (num2)
+//     res.send({message : resultado})
+// })
 
-// Dividir
-app.post ("/api/v1/div", (req, res) => {
-    const {num1, num2} = req.body
-    const resultado = Number(num1) / Number(num2)
-    res.send({message : resultado})
-})
+// // Dividir
+// app.post ("/api/v1/div", (req, res) => {
+//     const {num1, num2} = req.body
+//     const resultado = Number(num1) / Number(num2)
+//     res.send({message : resultado})
+// })
 
 
 // Dividir com GET
@@ -40,7 +35,6 @@ app.post ("/api/v1/div", (req, res) => {
 //    const resultado = Number(num1) / Number(num2)
 //    res.send({message : resultado})
 //})
-
     app.listen(3000, () => {
     console.log("serve in port 3000")
 })
